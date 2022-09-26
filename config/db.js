8b5272feccb99c30fad4ee10f044a4f558b715d6
jatin <khandelwaljatin2401@gmail.com>
2022-09-26 16:26:29 +0530

    message

--- config/db.js
+++ config/db.js
@@ -1,9 +1,11 @@
-const mongoose = require('mongoose')
+const mongoose=require("mongoose")
+
 const connectDatabase=()=>{
-    mongoose.connect(process.env.DB_URI,{useNewUrlParser:true,useUnifiedTopology:true}).then(
-        (data) => {
-            console.log(`Mongodb connected with server ${data.connection.host}`);
-        }
-    )
+mongoose.connect(process.env.DB_URI).then((data)=>{
+    console.log(`mongodb connected with server: ${data.connection.host}`)
+}).catch((err)=>{
+    console.log(err);
+})
 }
-module.exports =connectDatabase;
+
+module.exports= connectDatabase;
